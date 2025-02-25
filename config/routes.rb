609efd511fd359bab536889b88cc6e23eb190fc3
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-02-25 15:26:14 +0100

    exercice complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "tasks", to: "tasks#list", as: :tasks
-
-  get "tasks/new", to: "tasks#new", as: :new
-
-  get "tasks/:id", to: "tasks#show", as: :task
-
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
-
-  post "tasks", to: "tasks#create", as: :create
-
-  patch "tasks/:id", to: "tasks#update", as: :update
-
-  delete "tasks/:id", to: "tasks#delete", as: :delete
+  resources :tasks
 end
